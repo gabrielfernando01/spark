2fbf8c439f8b7773a85ef8c8c92c7514025d4f9f
Gabriel Fernando <fernando.rozepeda@gmail.com>
2022-04-09 12:36:50 -0500

    Add two directories

---
+++ PySpark/dataFrame.py
@@ -0,0 +1,28 @@
+# ==================================================================================================
+# Quickstart: DataFrame
+
+# Initializing SparkSession
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+# Create PySpark DataFrame from a list of rows
+
+from datatime import datatime, date
+import pandas as pd
+from pyspark.sql import Row
+
+df = spark.createDataFrame([
+    Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+    Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+    Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
+])
+df
+
+# Create a PySpark DataFrame with an explicit schema.
+df = spark.createDataFrame([
+    (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+    (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+    (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0)),
+], schema='a long, b double, c string, d date, e timestamp')
+df
+
